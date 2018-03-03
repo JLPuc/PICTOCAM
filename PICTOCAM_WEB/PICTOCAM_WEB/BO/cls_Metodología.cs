2192d0cb5a6d0555856f8798e6b4e9596e31b46c
Knoxez <p-iperezc-23@outlook.com>
2018-03-03 14:59:22 -0600

    Se agregó nuevo proyecto Web

---
+++ PICTOCAM_WEB/PICTOCAM_WEB/BO/cls_Metodología.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PICTOCAM_WEB.Models
+{
+    public class cls_Metodología
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private string escuela;
+    }
+}
