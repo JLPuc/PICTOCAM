7f3dbc4b6a9cf308948921271e0d67bc658eeeb8
Ameliavite <ameliavite@outlook.com>
2018-03-03 16:48:32 -0600

    Cambios de arquitectura
    Se agregaron las clases para los servicios web

---
+++ PICTOCAM_WEB/PICTOCAM_SERVICE/DAO/cls_PictogramaDAO.cs
@@ -0,0 +1,24 @@
+﻿using PICTOCAM_SERVICE.BO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace PICTOCAM_SERVICE.DAO
+{
+    public class cls_PictogramaDAO
+    {
+        cls_ConexionDAO conn;
+
+        public DataSet ver_Pictograma(object obj)
+        {
+            cls_Pictograma pictogramaBO = (cls_Pictograma)obj;
+
+            conn = new cls_ConexionDAO();
+            String sql = "EXEC PICTOGRAMACON "+pictogramaBO.Id;
+            return conn.TablaDS(sql);
+        }
+
+    }
+}
---
+++ PICTOCAM_WEB/PICTOCAM_SERVICE/Services/CtrlPictograma.cs
@@ -0,0 +1,35 @@
+﻿using PICTOCAM_SERVICE.BO;
+using PICTOCAM_SERVICE.DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace PICTOCAM_SERVICE.Services
+{
+    public class CtrlPictograma
+    {
+        cls_PictogramaDAO pictogramaDAO;
+        cls_Pictograma pictogramaBO;
+
+        #region "Servicios para la web"
+        //--------------Web--------------------------------
+       
+
+
+        #endregion
+
+        #region "Servicios para el movil"
+        //--------------Móvil--------------------------------
+        public DataSet ver_Pictograma(object pictogramaBO)
+        {
+            pictogramaDAO = new cls_PictogramaDAO();
+            return pictogramaDAO.ver_Pictograma(pictogramaBO);
+        }
+
+
+        #endregion
+
+    }
+}
